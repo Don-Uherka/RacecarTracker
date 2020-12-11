c380b1ced5354b14b23954cc39c165e3f259b9b3
Don-Uherka <uherkadon@yahoo.com>
2020-12-10 20:36:07 -0600

    Set up basic ASP.Net format

---
+++ Racecar_Tracker_By_UherkaMotorsports/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Racecar_Tracker_By_UherkaMotorsports.Data;
+
+[assembly: HostingStartup(typeof(Racecar_Tracker_By_UherkaMotorsports.Areas.Identity.IdentityHostingStartup))]
+namespace Racecar_Tracker_By_UherkaMotorsports.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
