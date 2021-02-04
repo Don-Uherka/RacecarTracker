5e403918f182a94fd645e50ee45fba568bf0c6f5
Don-Uherka <uherkadon@yahoo.com>
2021-02-03 21:15:47 -0600

    Added a model for a Crew member to add in data for the car

---
+++ Racecar_Tracker_By_UherkaMotorsports/Data/Migrations/20210204031449_Crew.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Racecar_Tracker_By_UherkaMotorsports.Data.Migrations
+{
+    public partial class Crew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Address1 = table.Column<string>(nullable: true),
+                    Address2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    Latitude = table.Column<decimal>(type: "decimal(10, 8)", nullable: false),
+                    Longitude = table.Column<decimal>(type: "decimal(11, 8)", nullable: false),
+                    Gears = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Track");
+        }
+    }
+}
---
+++ Racecar_Tracker_By_UherkaMotorsports/Models/Crew.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Racecar_Tracker_By_UherkaMotorsports.Models
+{
+    public class Crew
+    {
+        public int Id { get; set; }
+        public int Nights { get; set; }
+        public int Tires { get; set; }
+        public int Springs { get; set; }
+    }
+}
